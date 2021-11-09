dc22b56c6d110207750cca34f02eebcf58f782f0
Luiz Silva <luizsilva.ee@gmail.com>
2021-11-09 00:12:18 -0300

    Implementa adicionar, checar e excluir tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,15 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === ''){
+      return false;
+    }
+
+    const newTask = {
+      id: Math.random() * 100,
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task);
+    newTasks.forEach(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete
+      }
+    })
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    let newTasksList = tasks.map(task => task).filter(task => task.id !== id);
+
+    setTasks(newTasksList)
+    console.log(newTasksList);
   }
+
 
   return (
     <section className="task-list container">
